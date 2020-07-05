3e26cf015e0536f0990c753255e55b202297637d
Elias Puma <elpumitaelias@gmail.com>
2020-07-05 13:50:48 -0500

    Possibly last version of crawler

--- src/bin/crawl.rs
+++ src/bin/crawl.rs
@@ -33,7 +33,7 @@ fn request_stores(
     north: f64,
     east: f64,
 ) -> HashMap<i32, Store> {
-    let zoom = 20;
+    let zoom = 16;
     let country_code = "pe";
 
     let body = build_body(south, west, north, east, zoom, country_code);
@@ -46,33 +46,131 @@ fn request_stores(
 }
 
 fn store_response(conn: &PgConnection, result: HashMap<i32, Store>) {
+    let mut successful_inserts = 0;
+
     for store in result.values() {
         let insert_result = diesel::insert_into(stores::table)
             .values(store)
             .get_result::<Store>(conn);
 
         match insert_result {
-            Ok(_) => (),
+            Ok(_) => successful_inserts += 1,
             Err(_) => println!("[!] Error while saving store: {:?}", store),
         }
     }
+
+    print!("Inserted {} stores", successful_inserts);
 }
 
-fn main() {
+fn iterate_through_area(north: f64, east: f64, south: f64, west: f64) {
+    // Area Calc
+    let side_diff = 0.01575;
+    let mut coords_arr: Vec<(f64, f64, f64, f64)> = Vec::new();
+
+    let mut north_pivot = north;
+
+    while north_pivot < south {
+        let mut south_pivot = north_pivot + side_diff;
+        if south_pivot > south {
+            south_pivot = south;
+        }
+        let mut east_pivot = east;
+
+        while east_pivot < west {
+            let mut west_pivot = east_pivot + side_diff;
+            if west_pivot > west {
+                west_pivot = west;
+            }
+
+            coords_arr.push((north_pivot, east_pivot, south_pivot, west_pivot));
+            east_pivot = west_pivot;
+        }
+        north_pivot = south_pivot;
+    }
+
+    let areas_to_request = coords_arr.len();
+    println!("Areas to request: {}", areas_to_request);
+
     // Reqwest client
     let client = reqwest::blocking::Client::new();
     // Diesel client
     let connection = establish_connection();
+    // Requests and DB insertion
+    let mut n_request = 0;
+    for (north, east, south, west) in coords_arr {
+        n_request += 1;
+        let result = request_stores(&client, north, east, south, west);
+        print!("[r: {}/{}|North-B]\t", n_request, areas_to_request);
+        store_response(&connection, result);
+        println!("\t({}, {}, {}, {})", north, east, south, west);
+    }
+}
 
-    let south = -12.003392325421665;
-    let west = -77.09370711281127;
-    let north = -11.982277061323614;
-    let east = -77.06370928718871;
+fn main() {
+    // Reference
+    // let south = -12.07778082305759;
+    // let west = -76.9738632921875;
+    // let north = -12.062034945616542;
+    // let east = -76.9409043078125;
 
-    // let south = -12.233602;
-    // let west = -77.186818;
-    // let north = -11.857807;
-    // let east = -76.825380;
-    let result = request_stores(&client, south, west, north, east);
-    store_response(&connection, result);
+    // let south = -12.003392325421665;
+    // let west = -77.09370711281127;
+    // let north = -11.982277061323614;
+    // let east = -77.06370928718871;
+
+    // Lima
+    // let north = -12.2295735;
+    // let east = -77.1391195;
+    // let south = -11.947827;
+    // let west = -76.8175587;
+
+    // Peru
+    // let north = -18.2886053;
+    // let east = -82.6732716;
+    // let south = 0.7666798;
+    // let west = -68.3295077;
+
+    // South 1
+    // let north = -17.188226;
+    // let east = -72.572151;
+    // let south = -16.112174;
+    // let west = -68.717678;
+
+    // South 2
+    // let north = -17.399415;
+    // let east = -75.171298;
+    // let south = -17.188226;
+    // let west = -72.572151;
+
+    // Center 1
+    // let north = -17.188226;
+    // let east = -76.443410;
+    // let south = -13.163580;
+    // let west = -68.498418;
+
+    // Center 2
+    // let north = -13.163580;
+    // let east = -78.319916;
+    // let south = -9.751493;
+    // let west = -68.498418;
+
+    // North 1
+    // let north = -9.751493;
+    // let east = -79.750866;
+    // let south = -7.190257;
+    // let west = -71.983532;
+
+    // North A
+    // let north = -7.190257;
+    // let east = -81.673473;
+    // let south = -3.000069;
+    // let west = -76.685680;
+
+    // North B
+    let north = -7.190257;
+    let east = -76.685680;
+    let south = 0.030786;
+    let west = -69.720348;
+
+    iterate_through_area(north, east, south, west);
 }
